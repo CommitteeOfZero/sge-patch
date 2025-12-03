e97e463abe4542464f610371fa3eb2b974d898c8
Enorovan <enorovan@gmail.com>
2025-12-03 18:43:31 +0100

    S;GE 1.1.0

--- installer/userdata/README.txt
+++ installer/userdata/README.txt
@@ -1,6 +1,6 @@
-STEINS;GATE ELITE Improvement Patch
+STEINS;GATE ELITE Improvement Patch (Steam)
 Committee of Zero
-Version 1.0.0 - October 15, 2024
+Version 1.1.0 - December 14, 2025
 ==============================
 https://sonome.dareno.me
 https://twitter.com/CommitteeOf0
@@ -13,6 +13,14 @@ https://www.twitch.tv/CommitteeOfZero
 See https://sonome.dareno.me/projects/sge-patch.html for details, contact information and further instructions.
 
 For troubleshooting, please come to our Discord. You can always find the latest version of the patch at https://github.com/CommitteeOfZero/sge-patch/releases
+
+==============================
+Changes from 1.0.0:
+==============================
+
+* Switch compatibility.
+* Minor text and consistency fixes.
+* Fixed a bug where some empty lines were displayed.
 
 ==============================
 Credits: 
@@ -28,6 +36,9 @@ Hacking:
 * SomeAnon
 * daxxy
 * MrComputerRevo
+
+Switch Porting:
+* RubyX_Coded
 
 Translation:
 * Enorovan
---
+++ terribuild_switch.ps1
@@ -0,0 +1,63 @@
+﻿param ([string]$region)
+
+$version_string = "1.1.0"
+
+switch ($region) {
+    "EU" { $title_id = "010040000C596000"; $suffix = "eng"; break }
+    "US" { $title_id = "010042800B880000"; $suffix = "eng"; break }
+    Default {}
+}
+
+function PrintSection {
+    param ([string]$desc)
+    $line = "------------------------------------------------------------------------"
+    $len = (($line.length, $desc.legnth) | Measure -Max).Maximum
+    
+    Write-Host ""
+    Write-Host $line.PadRight($len) -BackgroundColor DarkBlue -ForegroundColor Cyan
+    Write-Host ("      >> " + $desc).PadRight($len) -BackgroundColor DarkBlue -ForegroundColor Cyan
+    Write-Host $line.PadRight($len) -BackgroundColor DarkBlue -ForegroundColor Cyan
+    Write-Host ""
+}
+
+Write-Output "                          ＴＥＲＲＩＢＵＩＬＤ"
+Write-Output "Rated World's #1 Build Script By Leading Game Industry Officials"
+Write-Output ""
+Write-Output "------------------------------------------------------------------------"
+Write-Output ""
+
+PrintSection "Creating new DIST and temp"
+Remove-Item -Force -Recurse -ErrorAction SilentlyContinue .\DIST
+New-Item -ItemType directory -Path .\DIST | Out-Null
+
+PrintSection "Pulling latest script changes"
+cd sge-scripts
+& git pull
+cd ..
+
+PrintSection "Patching scripts and copying to romfs"
+New-Item -ItemType directory -Path .\temp\patched_script_archive | Out-Null
+copy script_archive_switch\*.scx temp\patched_script_archive
+New-Item -ItemType directory -Path .\temp\patched_edited_script_archive | Out-Null
+copy temp\patched_script_archive\*.scx temp\patched_edited_script_archive
+.\sc3tools\target\release\sc3tools.exe replace-text temp\patched_edited_script_archive\*.scx sge-scripts\*.txt sge
+Copy-Item -Recurse -Force temp\patched_edited_script_archive .\DIST\atmosphere\contents\$title_id\romfs\script
+
+PrintSection "Copying assets to romfs"
+Copy-Item -Recurse .\c0data_switch_$suffix\* .\DIST\atmosphere\contents\$title_id\romfs
+
+PrintSection "Copying readme and license"
+Copy-Item -Recurse .\content_switch\* .\DIST\atmosphere\contents\$title_id
+
+PrintSection "Clean"
+Get-ChildItem -Path .\DIST -Include .gitkeep -Recurse | foreach { $_.Delete()}
+
+PrintSection "Packing the patch"
+$patchFolderName = "SGESwitch${region}Patch-v$version_string-Setup"
+cd .\DIST
+7z a -mx=5 "$patchFolderName.zip" "."
+Remove-Item -Force -Recurse .\atmosphere
+cd ..
+
+PrintSection "Removing temp"
+Remove-Item -Force -Recurse .\temp
